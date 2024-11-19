12c8a0d9ae559b50c3b65bef87b58502b40435dd
Casper L <cl@cply.dk>
2024-11-19 21:25:50 +0100

    Initial Commit

---
+++ PlayaroundApp/PlayaroundAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayaroundAppApp.swift
+//  PlayaroundApp
+//
+//  Created by Casper Lykke Andersen on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlayaroundAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlayaroundAppTests/PlayaroundAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlayaroundAppTests.swift
+//  PlayaroundAppTests
+//
+//  Created by Casper Lykke Andersen on 19/11/2024.
+//
+
+import Testing
+@testable import PlayaroundApp
+
+struct PlayaroundAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
